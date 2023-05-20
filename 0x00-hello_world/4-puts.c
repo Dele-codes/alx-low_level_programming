d3bab480b1d23fcccfa489faa911ad63beb93a13
Dele-codes <oladeleomole314@gmail.com>
2023-05-20 15:52:03 -0700

    cisfun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* 
+ * main - this is the entry point
+ *
+ * puts - prints to standardoutput
+ *
+ *return 0: success
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main -- the entry point
+ *
+ * description: prints to standard oupu with the printf
+ *
+ * return 0: success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return 0;
+}
