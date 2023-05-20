a1ac874ab138c6f68c3fade94b62906a0fdf34ea
Dele-codes <oladeleomole314@gmail.com>
2023-05-20 16:33:55 -0700

    debug err

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: prints the sizeof detat types
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int i;
+	long int b;
+	long long int c;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", (sizeof(a));
+	printf("Size of an int: %d byte(s)\n", (sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", (sizeof(b));
+	printf("Size of a long long int: %d byte(s)\n", (sizeof(c));
+	printf("Size of a float: %d byte(s)\n", (sizeof(f));
+	return (0);
+}
