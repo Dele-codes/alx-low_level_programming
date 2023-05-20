e1cd01df95d59b28bba002f847d9bf9c28484a2a
Dele-codes <oladeleomole314@gmail.com>
2023-05-20 16:08:40 -0700

    bug fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-/* 
+
+/**
  * main - this is the entry point
  *
- * puts - prints to standardoutput
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
- *return 0: success
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-/*
- * main -- the entry point
+
+/**
+ * main -Entry point
  *
- * description: prints to standard oupu with the printf
- *
- * return 0: success
+ * Description: prints "with proper grammar, but the outcome is a piece
+ * of art,"
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
